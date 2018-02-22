6368fd45dd8cca655fb175f87d6a5d5cf609bff5
Kaspars Aleksejevs <kaspars.aleksejevs@gmail.com>
2018-02-22 22:43:21 +0200

    Bumped library version

--- KnockoutMvcHelpers/KnockMvc/Properties/AssemblyInfo.cs
+++ KnockoutMvcHelpers/KnockMvc/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kaspars Aleksejevs")]
 [assembly: AssemblyProduct("KnockMvc")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.2.*")]
